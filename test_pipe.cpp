6b5d1384189f29a6e824b248425165a37f863c8e
Faridakim <101147363+Faridakim@users.noreply.github.com>
2022-03-15 12:51:37 +0100

    Add files via upload

---
+++ test_pipe.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+
+int pid = fork();
+
+if (pid < 0)
+{
+
+printf("Erreur de création du processus fils \n");
+exit(1);
+}
+if (pid == 0)
+{
+
+printf("Je suis le processus fils (%d), le pid de mon père est : (%d)\n", getpid(), getppid());
+}
+else
+{
+
+printf("Je suis le processus père (%d), le pid de mon fils est: (%d)\n", getpid(), pid);
+}
+return 0;
+}
